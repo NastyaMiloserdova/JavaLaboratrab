c32f50b0d157edbabb6f4ca4c2557e875aafb26b
NastyaMiloserdova <48564279+NastyaMiloserdova@users.noreply.github.com>
2020-06-06 01:53:30 +0300

    Шестая лабораторная работа

---
+++ laborat6/FractalGenerator.java
@@ -0,0 +1,48 @@
+import java.awt.geom.Rectangle2D;
+
+public abstract class FractalGenerator 
+{
+
+    /**
+     *
+     * @param rangeMin минимальное значение диапазона с плавающей точкой
+     * @param rangeMax максимальное значение диапазона с плавающей точкой
+     *
+     * @param size размер измерения, из которого координата пикселя. 
+     *      Например, это может быть ширина изображения или высота изображения.
+     *
+     * @param coord координата для вычисления значения двойной точности.
+     *      Координата должна находиться в диапазоне [0, размер].
+     */
+    public static double getCoord(double rangeMin, double rangeMax,
+        int size, int coord) 
+    {
+        assert size > 0;
+        assert coord >= 0 && coord < size;
+
+        double range = rangeMax - rangeMin;
+        return rangeMin + (range * (double) coord / (double) size);
+    }
+
+    public abstract void getInitialRange(Rectangle2D.Double range);
+
+    /**
+     * Обновляет текущий диапазон с центром в указанных координатах, 
+     * а также для увеличения или уменьшения с помощью 
+     * указанного коэффициента масштабирования.
+     */
+    public void recenterAndZoomRange(Rectangle2D.Double range,
+        double centerX, double centerY, double scale) 
+    {
+
+        double newWidth = range.width * scale;
+        double newHeight = range.height * scale;
+
+        range.x = centerX - newWidth / 2;
+        range.y = centerY - newHeight / 2;
+        range.width = newWidth;
+        range.height = newHeight;
+    }
+
+    public abstract int numIterations(double x, double y);
+}
